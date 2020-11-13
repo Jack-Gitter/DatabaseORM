3c836a1a3e2e9a7d146b2c6b017f2294e0bc5c60
Jack Gitter <gitter.j@husky.neu.edu>
2020-11-13 15:51:55 -0500

    finished code

---
+++ src/main/webapp/movies/movie-list.js
@@ -0,0 +1,48 @@
+class MovieList extends React.Component {
+
+  state = {
+    movies:[]
+  }
+
+  findAllMovies = () =>
+      findAllMovies().then(movies => this.setState({movies}))
+
+  createMovie = () =>
+      createMovie().then(this.findAllMovies)
+
+  deleteMovie = (id) =>
+      deleteMovie(id).then(this.findAllMovies)
+
+  componentDidMount = () =>
+      this.findAllMovies()
+
+  render() {
+    return(
+        <div>
+        <h1>Movie List</h1>
+          <table>
+            {
+              this.state.movies.map(movie =>
+              <tr key={movie.id}>
+                <td>
+                  {movie.title}
+                </td>
+                <td>
+                  <button onClick={() => this.deleteMovie(movie.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              )
+            }
+          </table>
+          <button onClick={this.createMovie}>
+            Create
+          </button>
+    </div>
+  )
+  }
+}
+
+ReactDOM.render(
+<MovieList/>, document.getElementById("root"))
